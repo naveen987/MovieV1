066949a8a8b5c5f12861c888afb0572b44f79f6d
naveen987 <tsnaveentumkur@gmail.com>
2020-10-08 02:50:35 +0530

    Updates

---
+++ precache-manifest.7f746263d6f170b64a86cec29aa06609.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d303bfddbf8a1863965eb016d91c70",
+    "url": "/MovieV1/index.html"
+  },
+  {
+    "revision": "598f59377cf99d7bdd60",
+    "url": "/MovieV1/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "f57ad53e2ea028296eb1",
+    "url": "/MovieV1/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "598f59377cf99d7bdd60",
+    "url": "/MovieV1/static/js/2.1f7e152a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/MovieV1/static/js/2.1f7e152a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f57ad53e2ea028296eb1",
+    "url": "/MovieV1/static/js/main.c462f3ba.chunk.js"
+  },
+  {
+    "revision": "817634ea9fdb188e9f28",
+    "url": "/MovieV1/static/js/runtime-main.d2af7e84.js"
+  }
+]);
